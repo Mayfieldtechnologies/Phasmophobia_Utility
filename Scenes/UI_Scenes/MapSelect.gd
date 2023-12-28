cbe745052299301e7473883a743828441b76f039
Mayfieldtechnologies <mayfieldtechnologies@gmail.com>
2023-12-28 14:49:08 -0600

    Broken - new map select

---
+++ Scenes/UI_Scenes/MapSelect.gd
@@ -0,0 +1,27 @@
+extends Control
+
+signal loadMap(selectedMap)
+
+@export var selected_map_value = ""
+@export var zIndex = 1
+@onready var MapDropdown = $MapDropdownButton
+#@onready var CurrMap = get_node("/root/Main/Current/Map")
+#@onready var locMapSpawn = get_node("/root/Main/locMapSpawn/locMapSpawnPointRight")
+
+func _on_pressed():
+	var selected_map_index = MapDropdown.selected
+	selected_map_value = MapDropdown.get_item_text(selected_map_index)
+	
+	LoadMap()
+
+func LoadMap():
+	var sceneMap = load("res://Scenes/Map_Scenes/" + selected_map_value.replace(" ","") +".tscn")
+	var instMap = sceneMap.instantiate()
+	
+	#CurrMap.add_child(instMap)
+	#instMap.global_position = locMapSpawn.global_position
+	#instMap.z_index = zIndex
+
+
+func _on_map_load_pressed():
+	emit_signal("loadMap",selected_map_value)
